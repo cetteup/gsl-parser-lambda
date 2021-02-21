9e0ace971366be6b03806c7170fe35245e8d527f
cetteup <me@cetteup.com>
2021-02-21 10:56:26 +0100

    Added a few comments

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ exports.lambdaHandler = async (event) => {
     };
 
     try {
-        // Make sure a channel name has been provided
+        // Make sure a game has been provided
         if (!event.pathParameters || !('game' in event.pathParameters) || !(event.pathParameters.game.trim() in GSL_URLS)) {
             response.statusCode = 422;
             throw new Error('No/invalid game name provided');
         }
 
+        // Fetch gsl
         const res = await fetch(GSL_URLS[event.pathParameters.game.trim()]);
         const gsl = await res.text();
+        // Parse gsl
         const servers = await parseGslFileContent(gsl);
+        // Use field filter if any fields have been specified
         const fields = event?.queryStringParameters?.fields ? event.queryStringParameters.fields.split(',') : null;
         response.body = JSON.stringify(servers, fields);
         response.statusCode = 200;
@@ -33,14 +36,19 @@ exports.lambdaHandler = async (event) => {
 };
 
 async function parseGslFileContent(gslFileContent) {
+    // Split into lines
     const lines = gslFileContent.trim().split('\n');
     let servers = [];
     for (const line of lines) {
+        // Line format "{ip}:{port} \{rawData}"" => split on " \" and get second element
         const rawData = line.split(' \\')[1];
+        // Data elements are separated by backslashes
         const elements = rawData.split('\\');
+        // Data format is "key\value\key\value[...]" => use even index elements as keys, uneven elements as values
         const keys = elements.filter((elem, i) => i % 2 == 0);
         const values = elements.filter((elem, i) => i % 2 == 1);
 
+        // Build server object (and cleanup up value), then add server to list
         servers.push(Object.fromEntries(keys.map((key, i) => [key, unescape(values[i].replace(/"/g, '')).trim()])));
     }
 
